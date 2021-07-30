a6b06eadabc4c80581923759961d0a932d33eb09
Omar Ibrahim <omar.ibrahim.991@gmail.com>
2021-07-30 22:25:32 +0200

    Fetching & showing data is completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [students, updateStudents] = useState([])
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		fetch("https://api.hatchways.io/assessment/students")
+		.then(resp => resp.json())
+		.then(({ students }) => {
+			updateStudents(students)
+			setLoading(false)
+		})
+	}, [])
+
+	return (
+		<div>
+			{
+				loading ?
+				<h1>Loading...</h1> :
+				students.map(({ firstName, lastName, email, company, skill, grades, pic }) => (
+					<div key={email} className="student-info">
+						<img src={pic} alt={`${firstName} ${lastName}'s profile`} />
+						<h2>{firstName + " " + lastName}</h2>
+						<p>{`Email: ${email}`}</p>
+						<p>{`Company: ${company}`}</p>
+						<p>{`Skill: ${skill}`}</p>
+						<p>
+							{`Average: ${(grades.reduce((a,b) => parseInt(a) + parseInt(b), 0) / grades.length).toFixed(2)}`}
+						</p>
+					</div>
+				))
+			}
+		</div>
+	)
 }
 
-export default App;
+export default App
